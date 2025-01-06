5b5fd2e6299995a317a903304a6f49a62be6eb33
Admin <Admin@DESKTOP-5IN5P6J>
2025-01-06 19:14:00 +0530

    fourth repo

---
+++ Assignment1/src/model1/Method1.java
@@ -0,0 +1,19 @@
+package model1;
+
+public class Method1 {
+
+	public static void main(String[] args) {
+	Method1.method1();
+	}
+	public static void method1()
+	{
+		int a=5;
+	int b=6;
+	int c=a+b;
+	System.out.println(c);
+
+
+	}
+
+}
+
---
+++ Assignment1/src/model1/Methods.java
@@ -0,0 +1,22 @@
+package model1;
+
+public class Methods {
+
+	public static void main(String[] args) {
+	Methods.method1();
+	Methods.method2();
+	}
+	public static void method1()
+	{
+		int a=6,b=2,sum;
+		sum=a+b;
+		System.out.println("The sumis" +sum);
+	}
+	public static void method2()
+	{
+		int a=6,b=2,mul;
+		mul=a*b;
+		System.out.println("The product is" + mul);
+	}
+
+}
